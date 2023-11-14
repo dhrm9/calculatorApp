28449ce861e5e1731f7c9c80d4be5bc0c805ec1d
dhrm9 <dharamendrasingh11@gmail.com>
2023-11-14 22:00:19 +0530

    added project

---
+++ lib/home_page.dart
@@ -0,0 +1,276 @@
+import 'package:calculatordemo/my_button.dart';
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var userInput = '';
+  var answer = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical:20.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      Text(userInput.toString(),
+                          style: TextStyle(fontSize: 30, color: Colors.white)),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Text(
+                        answer.toString(),
+                        style: TextStyle(fontSize: 30, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      MyButton(
+                        title: 'AC',
+                        onPress: () {
+                          userInput='';
+                          answer='';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: '+/-',
+                        onPress: () {
+                          userInput+='+/-';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: '%',
+                        onPress: () {
+                           userInput+='%';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: '/',
+                        color: Color(0xffffa00a),
+                        onPress: () {
+                           userInput+='/';
+                          setState(() {
+                            
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      MyButton(
+                        title: '7',
+                        onPress: () {
+                          userInput += '7';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: '8',
+                        onPress: () {
+                          userInput += '8';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: '0',
+                        onPress: () {
+                          userInput += '0';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: 'x',
+                        color: Color(0xffffa00a),
+                        onPress: () {
+                          userInput += 'x';
+                          setState(() {
+                            
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      MyButton(
+                        title: '4',
+                        onPress: () {
+                          userInput += '4';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: '5',
+                        onPress: () {
+                          userInput += '5';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: '6',
+                        onPress: () {
+                          userInput += '6';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: '-',
+                        color: Color(0xffffa00a),
+                        onPress: () {
+                          userInput += '-';
+                          setState(() {
+                            
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      MyButton(
+                        title: '1',
+                        onPress: () {
+                          userInput += '1';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: '2',
+                        onPress: () {
+                          userInput += '2';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: '3',
+                        onPress: () {
+                          userInput += '3';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: '+',
+                        color: Color(0xffffa00a),
+                        onPress: () {
+                          userInput += '+';
+                          setState(() {
+                            
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      MyButton(
+                        title: '0',
+                        onPress: () {
+                          userInput += '0';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: '.',
+                        onPress: () {
+                          userInput += '.';
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: 'DEL',
+                        onPress: () {
+                          userInput =userInput.substring(0,userInput.length-1);
+                          setState(() {
+                            
+                          });
+                        },
+                      ),
+                      MyButton(
+                        title: '=',
+                        onPress: () {
+                          equalPress();
+                        setState(() {
+                          
+                        });
+                        },
+                        color: Color(0xffffa00a),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      )),
+    );
+    
+  }
+  
+  void equalPress(){
+    String finalUserInput = userInput;
+    finalUserInput = userInput.replaceAll('x', '*');
+    Parser p = Parser();
+    Expression expression =p.parse(finalUserInput);
+    ContextModel contextModel = ContextModel();
+    double eval = expression.evaluate(EvaluationType.REAL,contextModel);
+    answer = eval.toString();
+    
+  }
+}
